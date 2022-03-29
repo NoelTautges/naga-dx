a66898159a7be6028e9b3401c47a68593f5f14c2
Noel Tautges <noelatautges@gmail.com>
2022-03-29 18:25:46 -0500

    Add test cases

---
+++ shaders/complex_input_output.hlsl
@@ -0,0 +1,17 @@
+struct vs_input {
+    float4 position : SV_Position;
+    float4 normal : NORMAL;
+};
+
+struct vs_output {
+    float4 position : SV_Position;
+    float4 color : COLOR;
+};
+
+vs_output VSMain(const vs_input input)
+{
+    vs_output output;
+    output.position = input.position;
+    output.color = input.normal;
+    return output;
+}
---
+++ shaders/input_sizes.hlsl
@@ -0,0 +1,10 @@
+struct vs_input {
+    float3 position : SV_Position;
+    float psize : PSIZE;
+    uint blend : BLENDINDICES;
+};
+
+float4 VSMain(const vs_input input) : SV_Position
+{
+    return float4(input.position.xy, input.position.z + input.psize, input.blend);
+}
