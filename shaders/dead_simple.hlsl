560aa91ba6e1e543cbfd4aaee12d58110ee11d21
Noel Tautges <noelatautges@gmail.com>
2022-04-08 16:17:18 -0500

    Add support for addressing temps and inputs, skip adding output struct if empty

---
+++ shaders/dead_simple.hlsl
@@ -0,0 +1,4 @@
+void VSMain()
+{
+    return;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,9 @@ use dxbc::dr::{shex::*, Operands, *};
 use naga::valid::{Capabilities, ModuleInfo, ValidationFlags, Validator};
 use naga::{
     Binding, BuiltIn, Constant, ConstantInner, EntryPoint, Expression, Function, FunctionArgument,
-    FunctionResult, GlobalVariable, Handle, Module, ResourceBinding, ScalarKind, ScalarValue,
-    ShaderStage, Span, Statement, StorageClass, StructMember, Type, TypeInner, VectorSize,
+    FunctionResult, GlobalVariable, Handle, LocalVariable, Module, ResourceBinding, ScalarKind,
+    ScalarValue, ShaderStage, Span, Statement, StorageClass, StructMember, Type, TypeInner,
+    VectorSize,
 };
 
 fn get_vector_size(size: usize) -> VectorSize {
@@ -40,6 +41,14 @@ fn get_scalar_width(imm: &Immediate) -> u8 {
     }
 }
 
+// TODO: better fails for bad bytecode
+fn get_first_immediate(op: &OperandToken0) -> u32 {
+    match op.get_immediate(0) {
+        Immediate::U32(n) => n,
+        _ => unreachable!(),
+    }
+}
+
 pub struct NagaConsumer {
     /// Module populated in [`finalize`].
     module: Module,
@@ -47,6 +56,8 @@ pub struct NagaConsumer {
     function: Function,
     /// Program type. Vertex, pixel, etc.
     program_ty: ProgramType,
+    /// Temporary registers as [`Expression::LocalVariable`]s.
+    temps: Vec<Handle<Expression>>,
 }
 
 impl NagaConsumer {
@@ -60,6 +71,7 @@ impl NagaConsumer {
             module,
             function,
             program_ty: ProgramType::Vertex,
+            temps: vec![],
         }
     }
 
@@ -126,7 +138,7 @@ impl NagaConsumer {
                 Binding::BuiltIn(semantic)
             };
 
-            // Delay constructing type because we need &inner for interpolation and sampling
+            // Type construction is delayed because we need &inner for interpolation and sampling
             let ty = Type {
                 // TODO: struct name
                 name: None,
@@ -149,11 +161,12 @@ impl NagaConsumer {
 
     fn get_variable_expression(&mut self, op: OperandToken0, span: Span) -> Handle<Expression> {
         let expr = match op.get_operand_type() {
+            OperandType::Input => {
+                let first = get_first_immediate(&op);
+                Some(Expression::FunctionArgument(first))
+            }
             OperandType::Output => {
-                let first = match op.get_immediate(0) {
-                    Immediate::U32(n) => n,
-                    _ => unreachable!(),
-                };
+                let first = get_first_immediate(&op);
                 // TODO: remove hardcoding
                 let global = GlobalVariable {
                     name: Some("Position".to_owned()),
@@ -175,7 +188,9 @@ impl NagaConsumer {
                     ),
                     init: None,
                 };
-                Expression::GlobalVariable(self.module.global_variables.append(global, span))
+                Some(Expression::GlobalVariable(
+                    self.module.global_variables.append(global, span),
+                ))
             }
             OperandType::Immediate32 => {
                 let imms = op.get_immediates();
@@ -191,7 +206,7 @@ impl NagaConsumer {
                         },
                     };
                     let const_handle = self.module.constants.fetch_or_append(c, span);
-                    Expression::Constant(const_handle)
+                    Some(Expression::Constant(const_handle))
                 } else {
                     let width = match first {
                         Immediate::U32(_) => 4,
@@ -234,12 +249,32 @@ impl NagaConsumer {
                         inner: ConstantInner::Composite { ty, components },
                     };
                     let handle = self.module.constants.fetch_or_append(c, span);
-                    Expression::Constant(handle)
+                    Some(Expression::Constant(handle))
                 }
+            }
+            _ => None,
+        };
+
+        if let Some(e) = expr {
+            return self.function.expressions.append(e, span);
+        }
+
+        let handle = match op.get_operand_type() {
+            OperandType::Temp => {
+                let i = match op.get_immediate(0) {
+                    Immediate::U32(n) => n,
+                    _ => unreachable!(),
+                };
+                Some(self.temps[i as usize])
             }
             _ => todo!(),
         };
-        self.function.expressions.append(expr, span)
+
+        if let Some(h) = handle {
+            h
+        } else {
+            todo!()
+        }
     }
 }
 
@@ -281,6 +316,14 @@ impl Consumer for NagaConsumer {
 
     fn consume_osgn(&mut self, osgn: &IOsgnChunk) -> Action {
         let s = self.get_elements(osgn);
+
+        // Skip adding output struct if it's empty
+        if let TypeInner::Struct { members, .. } = &s {
+            if members.is_empty() {
+                return Action::Continue;
+            }
+        }
+
         let ty = Type {
             name: None,
             inner: s,
@@ -294,7 +337,6 @@ impl Consumer for NagaConsumer {
 
     fn consume_instruction(&mut self, offset: u32, instruction: SparseInstruction) -> Action {
         dbg!(&instruction);
-        return Action::Continue;
         let span = Span::new(offset, offset + instruction.opcode.get_instruction_length());
 
         let statement = match instruction.operands {
@@ -309,7 +351,31 @@ impl Consumer for NagaConsumer {
             Operands::DclOutputSgv(_) => None,
             Operands::DclInputPsSiv(_) => None,
             Operands::DclInputPsSgv(_) => None,
-            Operands::DclTemps(_) => None,
+            Operands::DclTemps(dcl) => {
+                let four_floats = Type {
+                    name: None,
+                    inner: TypeInner::Vector {
+                        size: VectorSize::Quad,
+                        // TODO: determine specific type of temporary - analyze fields written to?
+                        kind: ScalarKind::Float,
+                        width: 4,
+                    },
+                };
+                let four_floats = self.module.types.insert(four_floats, span);
+                let len = self.function.expressions.len();
+                for i in 0..dcl.register_count {
+                    let var = LocalVariable {
+                        name: Some(format!("temp_{}", i)),
+                        ty: four_floats,
+                        init: None,
+                    };
+                    let var = self.function.local_variables.append(var, span);
+                    let var = Expression::LocalVariable(var);
+                    let var = self.function.expressions.append(var, span);
+                    self.temps.push(var);
+                }
+                Some(Statement::Emit(self.function.expressions.range_from(len)))
+            }
             Operands::DclIndexableTemp(_) => None,
             Operands::Add(_) => None,
             Operands::And(_) => None,
@@ -337,8 +403,14 @@ impl Consumer for NagaConsumer {
             Operands::Sample(_) => None,
             Operands::SampleL(_) => None,
             Operands::Ret => match &self.function.result {
-                Some(r) => None,
-                None => None,
+                Some(r) => {
+                    if let TypeInner::Struct { members, .. } = &self.module.types[r.ty].inner {
+                        None
+                    } else {
+                        None
+                    }
+                }
+                None => Some(Statement::Return { value: None }),
             },
             Operands::Unknown => None,
         };
@@ -351,7 +423,7 @@ impl Consumer for NagaConsumer {
     }
 
     fn finalize(&mut self) -> Action {
-        self.module.entry_points.push(EntryPoint {
+        let entry_point = EntryPoint {
             name: "main".to_owned(),
             stage: match self.program_ty {
                 ProgramType::Pixel => ShaderStage::Fragment,
@@ -362,7 +434,8 @@ impl Consumer for NagaConsumer {
             early_depth_test: None,
             workgroup_size: [0, 0, 0],
             function: take(&mut self.function),
-        });
+        };
+        self.module.entry_points.push(entry_point);
         Action::Continue
     }
 }
